93a82d1344994364a65e973a3ac53b4f2b742bf0
7in <shen5915@gmail.com>
2016-03-03 17:59:34 +0800

    增加json解析工具页面、js、css

---
+++ strategy-web/src/main/webapp/resources/assets/jsonUtil/head.js
@@ -0,0 +1,8 @@
+document.writeln('<script src="/resources/assets/jsonUtil/jquery.min.js" type="text/javascript"></script>');
+document.writeln('<script src="/resources/assets/jsonUtil/jquery-linedtextarea.js" type="text/javascript"></script>');
+document.writeln('<link href="/resources/assets/jsonUtil/jquery-linedtextarea.css" type="text/css" rel="stylesheet" />');
+document.writeln('<script type="text/javascript" src="/resources/assets/jsonUtil/jsl.parser.js"></script>');
+document.writeln('<script type="text/javascript" src="/resources/assets/jsonUtil/jsl.format.js"></script>');
+document.writeln('<script type="text/javascript" src="/resources/assets/jsonUtil/jsl.interactions.js"></script>');
+document.writeln('<link rel="stylesheet" href="/resources/assets/jsonUtil/screen.css" type="text/css"  media="screen, projection" />');
+document.writeln('<link rel="stylesheet" href="/resources/assets/jsonUtil/screen2.css" type="text/css"  media="screen, projection" />');
