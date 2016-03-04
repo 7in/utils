03afc54fb295f60d13c0d55e912aeaa90d9ff86e
7in <shen5915@gmail.com>
2016-03-04 09:52:08 +0800

    增加json解析工具页面controller

---
+++ strategy-web/src/main/java/com/simon7in/strategy/controllers/JsonFormatController.java
@@ -0,0 +1,19 @@
+package com.simon7in.strategy.controllers;
+
+import com.simon7in.strategy.BaseScreen;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class JsonFormatController extends BaseScreen{
+
+    @RequestMapping(value = {"/jsonFormat","/"},method = RequestMethod.GET)
+    public String welcome(ModelMap modelMap) {
+        super.getMessage();
+
+        return "/jsonFormat";
+    }
+
+}
